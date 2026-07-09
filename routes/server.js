4a8a7f61f4446391a8b8c3700fca001289bd6912
HelloThere2004 <binhcanguyen04@gmail.com>
2026-07-09 22:26:43 +0700

    Add back-up map feature to AWS S3 + Update Github Actions workflow

--- routes/server.js
+++ routes/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const serverCtrl = require('../controllers/serverCtrl');
+const backupCtrl = require('../controllers/backupCtrl');
 
 // POST /api/server/command
 router.post('/command', serverCtrl.executeCommand);
 
 // GET /api/server/logs
 router.get('/logs', serverCtrl.getLogs);
+
+//POST /api/server/backup
+router.post('/backup', backupCtrl.manualBackup);
 
 module.exports = router;
